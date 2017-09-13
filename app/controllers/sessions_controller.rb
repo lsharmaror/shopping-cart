844eed390fb1b387e0b3c05a31dd51588240aaa7
luv sharma <lsharma@grepruby.com>
2017-09-13 15:12:35 +0530

    Added Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to
+      redirect_to new_category_path
     else
       flash[:danger] = 'Invalid combination of email and password'
       render new
