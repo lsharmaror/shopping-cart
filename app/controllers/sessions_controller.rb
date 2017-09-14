6f915668b454dad4d77c837fb1b4a4a92cb42fd1
luv sharma <lsharma@grepruby.com>
2017-09-14 20:39:28 +0530

    Signup/Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,17 @@
 # Controller to handle sessions of all users
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to new_category_path
+    @user = User.find_by(email_id: params[:session][:email_id])
+    if @user == User.find_by(password: params[:session][:password])
+      log_in @user
+      redirect_to category_path
     else
       flash[:danger] = 'Invalid combination of email and password'
-      render new
+      render :new
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   # get 'home/index'
-  get 'sign_up', to: 'users#new'
-  post 'sign_up', to: 'users#create'
+  get 'sign-up', to: 'users#new'
+  post 'sign-up', to: 'users#create'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  get 'new_category', to: 'categories#new'
-  post 'new_category', to: 'categories#create'
-  delete 'delete_category', to: 'categories#destroy'
-  get 'new_product', to: 'product#new'
-  post 'new_product', to: 'product#create'
-  delete 'delete_product', to: 'product#destroy'
+  resources :products
+  resources :categories
   root 'home#index'
 end
